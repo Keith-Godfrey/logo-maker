1914d567709b15910fac4d6f6b74124f8954d7e2
Keith Godfrey <keith.godfrey23@gmail.com>
2024-06-25 16:15:45 -0700

    completed shapes.js, svg.js and shapes.test.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Required processes defined here.
 const fs = require('fs');
 const inquirer = require('inquirer');
-const shapes = require('./lib/shapes.js');
-
+const {Circle, Square, Trangle} = require('./lib/shapes.js');
+const SVG =require("./lib/svg")
 
 // create prompts for user for options they wat to use on their logo. Then call a fuction to render logo.
-const questions =[
     inquirer
         .prompt([
             {
@@ -21,13 +20,13 @@ const questions =[
             {
                 type: "input",
                 message: "please enter a color keyword or a hexadecimal number for the text color.",
-                name: "texColor",
+                name: "textColor",
             },
             {
-                type: "checkbox",
+                type: "list",
                 message: "please choose a shape for your logo.",
                 name: "logoShape",
-                choices: ["cirle", "square", "triangle"]            },
+                choices: ["circle", "square", "triangle"]            },
             {
                 type: "input",
                 message: "please enter a color keyword or a hexadecimal number for the color of the shape.",
@@ -37,10 +36,18 @@ const questions =[
         ])
 
         .then((res) => {
-            fs.writeToFile("./examples/logo.svg", shapes.renderLogo(res));
+        let shape;
+        if(res.logoShape ==="circle") shape = new Circle();
+        else if (res.logoShape ==="square") shape = new Square();
+        else shape = new Trangle();
+        shape.setColor(res.shapeColor);
+        const svg = new SVG();
+        svg.setText(res.logoText, res.textColor);
+        svg.setShape(shape);
+
+            writeToFile("examples/logo.svg", svg.render());
         }
         )
-]
 
 //This function writes the svg information to a file
 function writeToFile(fileName, data) {
